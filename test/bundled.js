ac0fa3691232d1e50a79bb46aededcd32d347148
bastengao <bastengao@gmail.com>
2019-11-22 10:18:56 +0800

    Support year 2020

--- test/bundled.js
+++ test/bundled.js
@@ -3,7 +3,7 @@ const Bundled = require('../lib/bundled');
 
 describe('Bundled', () => {
   it('loadEvents', () => {
-    should.equal(Bundled.loadEvents().length, 48);
+    should.equal(Bundled.loadEvents().length, 61);
 
     const days = Bundled.loadEvents()[0].days();
     should.equal(days.length, 3);
